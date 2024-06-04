d84888a5c7326fb8669d3c320158d58ef488896d
Deniz Unal <salihdenizunal@gmail.com>
2024-06-04 22:06:57 +0300

    Update comments in main

--- src/main.py
+++ src/main.py
@@ -3,52 +3,49 @@ import time
 import matplotlib.pyplot as plt
 from DynamicMaze import DynamicMaze
 
-# Main function
 def main(rows, cols, pawnSpeed, updateFactor):
-    # Use the TkAgg backend
-    plt.switch_backend('TkAgg')
 
-    # Initialize dynamic maze
+    # Initialize dynamic maze.
     dynamicMaze = DynamicMaze(rows, cols)
     
-    # Plot the initial maze
+    # Plot the initial maze.
     dynamicMaze.plot()
 
-    # Get the Tk window and maximize it
     figManager = plt.get_current_fig_manager()
     figManager.window.state('zoomed')
-    plt.pause(1)  # Add a small pause to allow for plot updates
+    # Add a small pause for the windws to be arranged.
+    plt.pause(1)
     
-    # Normalize pawn speed to ensure it's between 0 and 1
+    # Normalize pawn speed to ensure it's between 0 and 1.
     pawnSpeed = max(0.0000001, min(1, pawnSpeed))
     
-    # Normalize update factor to ensure it's greater than or equal to 1
-    updateFactor = max(1, updateFactor)
+    # Normalize update factor to ensure it's greater than or equal to 1.
+    updateFactor = max(0, updateFactor)
 
-    # Calculate the number of iterations between each movement based on pawn speed
-    iterationsPerMove = int(1 / pawnSpeed)
+    # Calculate the number of cycles between each movement based on pawn speed
+    cyclesPerMove = int(1 / pawnSpeed)
 
-    # Main loop
     counter = 0
     while True:
-        # Update maze
-        dynamicMaze.updateMaze(updateFactor)  # Update maze every iteration
+        # Update maze and move pawn every cyclesPerMove cycles. Plot the resulting
+        # maze and panws position after each update.
+        dynamicMaze.updateMaze(updateFactor)
         counter += 1
-        if counter % iterationsPerMove == 0:
+        if counter % cyclesPerMove == 0:
             dynamicMaze.pawn.move()
         dynamicMaze.plot()
 
-        # Pause briefly to control the simulation speed
+        # Pause briefly to control the simulation speed.
         time.sleep(0.01)
 
 if __name__ == "__main__":
-    # Parse command-line arguments
+    # Parse command-line arguments.
     parser = argparse.ArgumentParser(description='Dynamic Maze Solver')
     parser.add_argument('--rows', type=int, default=12, help='Number of rows in the maze')
     parser.add_argument('--cols', type=int, default=12, help='Number of columns in the maze')
-    parser.add_argument('--pawnSpeed', type=float, default=0.33333, help='Speed of the pawn (in seconds per move)')
-    parser.add_argument('--updateFactor', type=int, default=5, help='Factor of the updates.')
+    parser.add_argument('--pawnSpeed', type=float, default=0.33333, help='Speed of the pawn')
+    parser.add_argument('--updateFactor', type=int, default=5, help='Factor of the updates')
     args = parser.parse_args()
 
-    # Call main function with parsed arguments
+    # Call main function with parsed arguments.
     main(args.rows, args.cols, args.pawnSpeed, args.updateFactor)
